4bb2f7d0f5071b932714f200ca35ccc047d64d73
Jadom <matquadev@gmail.com>
2021-10-31 21:29:31 -0300

    Responsive menu working

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,27 @@ import { Squash as Hamburger } from 'hamburger-react';
 import './Nav.scss'
 import { useEffect, useState } from "react";
 import ResponsiveNav from "./ResponsiveNav";
+import { useMediaQuery } from "react-responsive";
 
 export default function Nav() {
     const [isOpen, setIsOpen] = useState(false);
+
+    const visibilityResponsiveMenu = useMediaQuery({
+        query: '(max-width: 768px)'
+    })
 
     return (
         <>
         <header>
                 <h3 className="logo">MOON<span className='logoGallery'>Gallery</span></h3>
-                <div className="toggle-menu">
-                    <Hamburger color="#e99001" toggle={setIsOpen} toggled={isOpen}/>
-                </div>
                 {
-                    isOpen ||
+                    visibilityResponsiveMenu &&
+                    <div className="toggle-menu">
+                        <Hamburger color="#e99001" toggle={setIsOpen} toggled={isOpen}/>
+                    </div>
+                }
+                {
+                    visibilityResponsiveMenu ||
                     <nav className="navigation">
                     <NavLink exact to="/" className="navLink" activeClassName="activeNavLink">Home</NavLink>
                     <NavLink exact to="/upload" className="navLink" activeClassName="activeNavLink">Upload</NavLink>
@@ -24,7 +32,7 @@ export default function Nav() {
                 }
         </header>
         {
-            isOpen  && <ResponsiveNav/> 
+            isOpen  && <ResponsiveNav isOpen={isOpen}/> 
         }
         </>
     )
--- src/components/ResponsiveNav.jsx
+++ src/components/ResponsiveNav.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useRef } from "react"
 import { NavLink } from "react-router-dom"
 import './ResponsiveNav.scss'
 
-export default function ResponsiveNav() {
+export default function ResponsiveNav({isOpen}) {
+    const navigation = useRef(null);
+    useEffect(()=>{
+        if(isOpen){
+            navigation.current.style.height = "auto"
+        }
+    })
     return (
-        <nav className="responsiveNavigation">
+        <nav className="responsiveNavigation" ref={navigation}>
             <NavLink exact to="/" className="navLinkRes" activeClassName="activeNavLinkRes">Home</NavLink>
             <NavLink exact to="/upload" className="navLinkRes" activeClassName="activeNavLinkRes">Upload</NavLink>
             <NavLink exact to="/login" className="navLinkRes" activeClassName="activeNavLinkRes">Login</NavLink>
