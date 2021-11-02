e84c943117034964d92d6eb9848a31396375e6ba
Jadom <matquadev@gmail.com>
2021-11-02 15:23:11 -0300

    404 error style is created

--- src/components/Err404.jsx
+++ src/components/Err404.jsx
@@ -1,8 +1,9 @@
+import './Err404.scss'
 export default function Err404() {
     return (
-        <div>
-            <h2>Error 404</h2>
-            <p>Page Not Found</p>
+        <div className="containerError">
+            <h2 className="error"><span>4</span> <img src="https://png2png.com/wp-content/uploads/2021/06/moon-png1-1024x1024.png" alt="" /> <span>4</span></h2>
+            <p className="msg">Page Not Found</p>
         </div>
     )
 }
