05f8a8a452edc249589bf13555d344f49d186b58
Jadom <matquadev@gmail.com>
2021-11-02 21:13:43 -0300

    index backend is updated

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 (async ()=> {
-    const db = await mongoose.connect('mongodb://localhost/moongallery', {
+    const db = await mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/moongallery', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
