9e27dd8fcd3d440b6b9e3dec080ba2afe789fb4c
Jadom <matquadev@gmail.com>
2021-11-07 16:55:29 -0300

    welcome animation created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Gallery from "../components/Gallery";
 import './Home.scss'
 
 export default function Home() {
+    const [loading, setLoading] = useState(false);
     return (
         <>
         <div className="containerHome">
             <div className="titleBox">
-                <h2 className='welcome'>Welcome to the <span>Moon Gallery</span></h2>
+                {
+                    loading || <h2 className='welcome'>Welcome to the <span>Moon Gallery</span></h2>
+                }
             </div>
-            <Gallery/>
+            <Gallery loading={loading} setLoading={setLoading}/>
         </div>
         </>
     )
