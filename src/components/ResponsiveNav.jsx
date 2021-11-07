83a966704a29723086063448e2d291c22368b0ff
Jadom <matquadev@gmail.com>
2021-11-06 21:36:39 -0300

    responsive nav is updated

--- src/components/ResponsiveNav.jsx
+++ src/components/ResponsiveNav.jsx
@@ -2,18 +2,41 @@ import { useEffect, useRef } from "react"
 import { NavLink } from "react-router-dom"
 import './ResponsiveNav.scss'
 
-export default function ResponsiveNav({isOpen}) {
+export default function ResponsiveNav({isOpen, setIsOpen}) {
     const navigation = useRef(null);
+
+
     useEffect(()=>{
         if(isOpen){
             navigation.current.style.height = "auto"
         }
     })
+
+    const handleClick = ()=> setIsOpen(null);
+
     return (
         <nav className="responsiveNavigation" ref={navigation}>
-            <NavLink exact to="/" className="navLinkRes" activeClassName="activeNavLinkRes">Home</NavLink>
-            <NavLink exact to="/upload" className="navLinkRes" activeClassName="activeNavLinkRes">Upload</NavLink>
-            <NavLink exact to="/login" className="navLinkRes" activeClassName="activeNavLinkRes">Login</NavLink>
+            <NavLink
+            exact
+            to="/"
+            className="navLinkRes" 
+            activeClassName="activeNavLinkRes"
+            onClick={handleClick}
+            >Home</NavLink>
+            <NavLink
+             exact
+             to="/upload"
+             className="navLinkRes"
+             activeClassName="activeNavLinkRes"
+             onClick={handleClick}
+            >Upload</NavLink>
+            <NavLink
+             exact
+             to="/login"
+             className="navLinkRes"
+             activeClassName="activeNavLinkRes"
+             onClick={handleClick}
+            >Login</NavLink>
         </nav>
     )
 }
