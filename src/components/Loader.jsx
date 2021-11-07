9c5637882c4db536139786ff194d7110956c9154
Jadom <matquadev@gmail.com>
2021-11-07 16:28:47 -0300

    loader and form page is now responsive

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,17 @@
 import { PropagateLoader } from "react-spinners"
+import { useMediaQuery } from "react-responsive";
 import './Loader.scss'
 
 export default function Loader() {
+    const sizeLoader = useMediaQuery({
+        query: '(max-width: 768px)'
+    })
     return (
         <div className='loader'>
             <PropagateLoader
-            size={25}
+            size={sizeLoader ? 20 : 25}
             color={'#ff9100'}
+            style={sizeLoader && {height: '200px', width:'200px'}}
             />
         </div>
     )
